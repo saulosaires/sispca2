e7d91a05c93104028db1dc242802ab99fcaaa279
DESKTOP-HV1U4MC\Saulo <saulosaires@gmail.com>
2018-06-11 14:47:04 -0300

    filtro por exercicio na tela  Fisicofinanceiro Anual

--- src/main/java/quantitativo/beans/fisicofinanceiro/anual/FisicoFinanceiroAnualListMBean.java
+++ src/main/java/quantitativo/beans/fisicofinanceiro/anual/FisicoFinanceiroAnualListMBean.java
@@ -7,7 +7,10 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import administrativo.model.Exercicio;
+import administrativo.model.Ppa;
 import administrativo.model.Usuario;
+import administrativo.service.PpaService;
 import arquitetura.utils.Messages;
 import arquitetura.utils.SessionUtils;
 import arquitetura.utils.SispcaLogger;
@@ -40,6 +43,14 @@ public class FisicoFinanceiroAnualListMBean implements Serializable {
 	
 	private List<Acao>listAcoes;
  
+	private Long ppaId;
+	private List<Ppa> listPpa;
+	
+	private Long exercicioId;
+	private List<Exercicio> listExercicio;
+
+	
+	private  PpaService ppaService;
     private AcaoService acaoService;
     
     private Boolean atualizar;
@@ -47,16 +58,24 @@ public class FisicoFinanceiroAnualListMBean implements Serializable {
     
 	@Inject
 	public FisicoFinanceiroAnualListMBean(AcaoService acaoService ,
+										  PpaService ppaService,
 										  UnidadeOrcamentariaService  unidadeOrcamentariaService, 
 										  ProgramaService programaService) {
 		
-		this.acaoService   = acaoService;
+		this.acaoService = acaoService;
+		this.ppaService  = ppaService;
 		
 		Usuario user = (Usuario) SessionUtils.get(SessionUtils.USER);
 		
 		this.listUnidadeOrcamentaria = unidadeOrcamentariaService.findAllOrderByDescricao(user.getId());
 		this.listPrograma 		     = programaService.findAllOrderByDenominacao();
  		
+		listPpa = ppaService.findAll();
+		
+		initPpa();
+		initExercicio();
+
+		
 		atualizar = SessionUtils.containsKey("planejamentoQuantitativoFisicoFinanceiroAnualAtualizar");
 		relatorio = SessionUtils.containsKey("planejamentoQuantitativoFisicoFinanceiroAnualRelatorio");
 		
@@ -69,7 +88,7 @@ public class FisicoFinanceiroAnualListMBean implements Serializable {
 
 		try {
 			
-			listAcoes = acaoService.buscar(codigo, denominacao, unidadeOrcamentariaId, programaId,null);
+			listAcoes = acaoService.buscar(codigo, denominacao, unidadeOrcamentariaId, programaId,exercicioId);
 
 			if(listUnidadeOrcamentaria.isEmpty()) {
 				Messages.addMessageWarn(NO_RECORDS);
@@ -83,7 +102,48 @@ public class FisicoFinanceiroAnualListMBean implements Serializable {
 		}
 	}
 
+    public void onChangePpa() {
+    	
+    	if(ppaId!=null) {
+    		listExercicio = ppaService.findById(ppaId).getExercicios();
+    	}else {
+    		exercicioId=null;
+    		listExercicio=null;	
+    	}
+    	
+    }
+	
+	
+	public void initPpa() {
+		
+		for(Ppa ppa: listPpa) {
+			if(ppa.getVigente()) {
+				ppaId = ppa.getId();
+				break;
+			}
+		}
+		
+	}
+	
+	public void initExercicio() {
+	
+		if(ppaId!=null) {
+			listExercicio = ppaService.findById(ppaId).getExercicios();
+			
+			for(Exercicio e: listExercicio) {
+				
+				if(e.getVigente()) {
+					exercicioId = e.getId();
+					break;
+				}
+				
+			}
+			
+		}
+		
+	}
 
+	
 	
 	public String getDenominacao() {
 		return denominacao;
@@ -172,6 +232,46 @@ public class FisicoFinanceiroAnualListMBean implements Serializable {
 
 	public void setRelatorio(Boolean relatorio) {
 		this.relatorio = relatorio;
+	}
+
+
+	public Long getPpaId() {
+		return ppaId;
+	}
+
+
+	public void setPpaId(Long ppaId) {
+		this.ppaId = ppaId;
+	}
+
+
+	public List<Ppa> getListPpa() {
+		return listPpa;
+	}
+
+
+	public void setListPpa(List<Ppa> listPpa) {
+		this.listPpa = listPpa;
+	}
+
+
+	public Long getExercicioId() {
+		return exercicioId;
+	}
+
+
+	public void setExercicioId(Long exercicioId) {
+		this.exercicioId = exercicioId;
+	}
+
+
+	public List<Exercicio> getListExercicio() {
+		return listExercicio;
+	}
+
+
+	public void setListExercicio(List<Exercicio> listExercicio) {
+		this.listExercicio = listExercicio;
 	}
 
 	
